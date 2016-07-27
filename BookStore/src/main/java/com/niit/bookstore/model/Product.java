87782b4b84c20288ae975e6a9177a92bd26e1339
srushtiy <srushtiy@yahoo.com>
2016-07-27 17:08:48 +0530

    Add files via upload

---
+++ BookStore/src/main/java/com/niit/bookstore/model/Product.java
@@ -0,0 +1,34 @@
+package com.niit.bookstore.model; 
+
+public class Product {
+	private String id;
+	private String name;
+	private String description;
+	private int quantity;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+
+}
